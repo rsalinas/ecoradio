532134031abde105cd6f8856eacc14eaf3b3af4e
Raul Salinas-Monteagudo <rausalinas@gmail.com>
2016-03-13 10:21:57 +0100

    Clean RSS parser. Now using exceptions

--- src/util/rssparser.cpp
+++ src/util/rssparser.cpp
@@ -4,82 +4,54 @@
 #include <QStack>
 #include <QDebug>
 
-class Item
-{
-public:
-};
-
-
-class MyXmlContentHandler : public QXmlDefaultHandler
-{
-    int indent = 0;
-    QStack<QString> stack;
-    RssParser &m_parent;
-public:
-    QString printIndent() {
-        return QString::fromStdString(std::string(indent*4, ' '));
-    }
-    MyXmlContentHandler(RssParser &parent) : m_parent(parent)
-    {
-    }
-
-    ~MyXmlContentHandler()
-    {
-    }
-
-    bool startElement(const QString & namespaceURI, const QString & localName,
-                      const QString & qName, const QXmlAttributes & atts ) override
-    {
-        stack.push(qName);
-        //        qDebug() << localName << "STACK: "<< stack;
-
-        QString url;
-        int size = 0;
-
-        for(int index = 0 ; index < atts.length();index++)
-        {
-            //            qDebug() <<printIndent() << atts.type(index)<< "=" << atts.value(index) << atts.qName(index);
-            if (stack.top() == "enclosure" && atts.type(index) == "CDATA") {
-                if (atts.qName(index) == "url") {
-                    url = atts.value(index);
-                } else if (atts.qName(index) == "length") {
-                    size = atts.value(index).toInt();
-                }
-            }
-        }
-        if (url.size()) {
-            //            qDebug() << "MP3: " << url << size;
-            //            m_stream
-            m_parent.m_streams.push_back(url);
-        }
-
-        //        qDebug() <<printIndent()<< "------------------------" ;
-        indent ++;
-        return true;
-    };
-    bool endElement(const QString & namespaceURI, const QString & localName, const QString & qName) override {
-        indent--;;
-        stack.pop();
-        //        qDebug() << stack;
-        return true;
-    }
-};
-
 RssParser::RssParser(const QString &filename)
 {
     QXmlSimpleReader parser;
-    MyXmlContentHandler handler(*this);
-    
-    parser.setContentHandler(&handler);
+    parser.setContentHandler(this);
     QFile f(filename);
     QXmlInputSource source(&f);
     if (parser.parse(&source))     {
     }     else     {
-        qDebug() <<"Parsing Failed..."; //FIXME
+        qDebug() << " " << filename;
+        throw RssException("RSS Parsing Failed " + filename);
     }
 }
 
 
 QStringList RssParser::getStreams() {
     return m_streams;
+}
+
+
+bool RssParser::startElement(const QString & namespaceURI, const QString & localName,
+                  const QString & qName, const QXmlAttributes & atts )
+{
+    stack.push(qName);
+    //        qDebug() << localName << "STACK: "<< stack;
+
+    QString url;
+    int size = 0;
+
+    for(int index = 0 ; index < atts.length();index++)
+    {
+        //            qDebug() <<printIndent() << atts.type(index)<< "=" << atts.value(index) << atts.qName(index);
+        if (stack.top() == "enclosure" && atts.type(index) == "CDATA") {
+            if (atts.qName(index) == "url") {
+                url = atts.value(index);
+            } else if (atts.qName(index) == "length") {
+                size = atts.value(index).toInt();
+            }
+        }
+    }
+    if (url.size()) {
+        m_streams.push_back(url);
+    }
+
+    indent ++;
+    return true;
+}
+bool RssParser::endElement(const QString & namespaceURI, const QString & localName, const QString & qName) {
+    indent--;;
+    stack.pop();
+    return true;
 }
--- src/util/rssparser.h
+++ src/util/rssparser.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#include <exception>
+
 #include <QStringList>
 #include <QtXml/QtXml>
+
 
 class RssParser : private QXmlDefaultHandler
 {
 public:
+    class RssException : public std::exception {
+    public:
+        RssException(const QString &cause) : m_cause(cause.toStdString()) {
+        }
+
+        const char * what() const noexcept(true) {
+            return m_cause.c_str();
+        }
+
+    private:
+        std::string m_cause;
+    };
+
     RssParser(const QString &filename);
     QStringList getStreams();
 
 private:
+    bool startElement(const QString & namespaceURI, const QString & localName,
+                      const QString & qName, const QXmlAttributes & atts ) override;
+    bool endElement(const QString & namespaceURI, const QString & localName, const QString & qName) override;
+    int indent = 0;
+    QString printIndent() {
+        return QString::fromStdString(std::string(indent*4, ' '));
+    }
+    QStack<QString> stack;
     QStringList m_streams;
-    friend class MyXmlContentHandler;
 };
