86dcdd31d5dce5478fb26db2d1b5950267522c41
Raul Salinas-Monteagudo <rausalinas@gmail.com>
2016-03-13 12:53:56 +0100

    Finish core&ui

---
+++ test/coreandui.cpp
@@ -0,0 +1,25 @@
+
+#include <QApplication>
+
+#include "radioui/radioconsole.h"
+#include "core/ecoradio.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Ecoradio * ecoradio = new Ecoradio(&app);
+
+    // This will cause the application to exit when
+    // the task signals finished.
+    QObject::connect(ecoradio, SIGNAL(finished()), &app, SLOT(quit()));
+
+    // This will run the task from the application event loop.
+    QTimer::singleShot(0, ecoradio, SLOT(run()));
+
+
+    RadioConsole w;
+    w.show();
+
+    return app.exec();
+}
