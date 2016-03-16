e91862983530a8965a72c4bbdef971488e1cf025
Raul Salinas-Monteagudo <rausalinas@gmail.com>
2016-03-16 20:28:37 +0100

    Move SQL schema to database/ and sample data to samples/

--- samples/radiomalva.sql
+++ samples/radiomalva.sql
@@ -1,10 +1,3 @@
-PRAGMA foreign_keys=ON;
-drop table if exists program ;
-create table program(ptype string, name string, path string, length int);
-drop table if exists program_time;
-create table program_time(program_id int not null, dow int, hour int, minute int, live int);
-drop table if exists program_instance;
-create table program_instance(program_id int not null, ts datetime, int program_number);
 begin;
 insert into program values ("live", "Radioflautas", null, 120);
 insert into program values ("live", "Tofuria", null, null);
